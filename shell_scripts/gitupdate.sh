6a217496e7af08ac1f990d483d6dfdc4da7bc9a1
CCOSTAN <carlo@ipm.com>
2016-10-14 14:58:33 +0000

    Defaults!

--- shell_scripts/gitupdate.sh
+++ shell_scripts/gitupdate.sh
@@ -6,7 +6,7 @@ hass --script check_config
 
 git add .
 git status
-echo -n "Enter the Description for the Change: "
+echo -n "Enter the Description for the Change: " [Minor Update]
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
