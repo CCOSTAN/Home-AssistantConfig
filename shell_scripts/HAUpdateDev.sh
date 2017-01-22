ed4ea3a7018fe3f9aeb80427fb8b00c7fb6b1b18
CCOSTAN <carlo@ipm.com>
2017-01-22 03:13:43 +0000

    General Housekeeping

---
+++ shell_scripts/HAUpdateDev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Setting up Environment"
+cd /home/hass/.homeassistant
+source /srv/hass/hass_venv/bin/activate
+
+echo "Processing update"
+pip3 install --upgrade git+git://github.com/balloob/home-assistant.git@dev
+
+echo "Code Check"
+hass --script check_config
+exit
