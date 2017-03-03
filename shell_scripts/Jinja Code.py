47bcd0b3123550a172a630ed58d2d4b25be31c1d
CCOSTAN <carlo@ipm.com>
2017-03-03 20:10:56 +0000

    #96 in Progress - Adding in MP3s.

--- shell_scripts/Jinja Code.py
+++ shell_scripts/Jinja Code.py
@@ -1,7 +1,7 @@
 You can use this code to quickly create files from the template editor in HA.  I use it mainly for `emulated_hue` and to quickly add in new Customize options to all things HA!
 
 
-For the sandbox. 
+For the sandbox.
 {% set trigger = {'entity_id':'sensor.downstairs_thermostat_hvac_state','to_state':'cooling'} %}
 
 #########################################################
@@ -10,7 +10,7 @@ Create fast Customize for groups, sensors, covers etc...  Just change that first
 {% for state in states.sensor -%}
   {% if loop.first %}
 {% elif loop.last %}
-{% else %} 
+{% else %}
 {% endif %}
 {{- state.entity_id }}:
   friendly_name: '{{ state.attributes.friendly_name|replace("_"," ",)|title() if state.attributes.friendly_name is defined else state.name|replace("_"," ",)|title() }}'
@@ -18,6 +18,7 @@ Create fast Customize for groups, sensors, covers etc...  Just change that first
   hidden: {{state.attributes.hidden if state.attributes.hidden is defined else "False"}}
   {{'icon: '+ state.attributes.icon if state.attributes.icon is defined}}
   {{'homebridge_cover_type: '+ state.attributes.homebridge_cover_type if state.attributes.homebridge_cover_type is defined}}
+  {{'assumed_state: '+ state.attributes.assumed_state if state.attributes.assumed_state is defined}}
 {% endfor -%}
 
 #########################################################
