10a1210d58654ac2a65e025e86535f38c68e8cd6
CCOSTAN <carlo@ipm.com>
2017-03-29 15:38:44 +0000

    FIXED THE ha STARTUP TIMES.  Added jinja code from @skalavala

--- shell_scripts/Jinja Code.py
+++ shell_scripts/Jinja Code.py
@@ -1,6 +1,5 @@
 You can use this code to quickly create files from the template editor in HA.  I use it mainly for `emulated_hue` and to quickly add in new Customize options to all things HA!
 
-
 For the sandbox.
 {% set trigger = {'entity_id':'sensor.downstairs_thermostat_hvac_state','to_state':'cooling'} %}
 
@@ -20,5 +19,25 @@ Create fast Customize for groups, sensors, covers etc...  Just change that first
   {{'homebridge_cover_type: '+ state.attributes.homebridge_cover_type if state.attributes.homebridge_cover_type is defined}}
   {{'assumed_state: '+ state.attributes.assumed_state if state.attributes.assumed_state is defined}}
 {% endfor -%}
+
+#########################################################
+#This code lists out EVERY possible entity and attribute for that entity.
+# source: https://github.com/skalavala/smarthome/blob/master/Entities.md
+#########################################################
+
+{{ "_".ljust(90, "_") }}
+{%- set domains = [states.light, states.switch, states.automation, states.device_tracker, states.group, states.media_player, states.proximity, states.script, states.zone, states.zwave, states.sensor, states.calendar ] %}
+{{ "Entity ID".ljust(50) }}{{ "Entity Name" }}
+{%- for domain in domains -%}
+{% for item in domain %}
+{{ "_".ljust(90, "_") }}
+{{ item.entity_id.ljust(50) }}{{ item.name }}
+{% for attrib in item.attributes %}
+{%- if attrib is defined %}
+  {{attrib.ljust(50)}}: {{ item.attributes[attrib] }}
+{%- endif %}
+{%- endfor %}
+{%- endfor %}
+{%- endfor %}
 
 #########################################################
